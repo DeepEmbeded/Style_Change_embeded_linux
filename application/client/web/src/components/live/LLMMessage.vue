93af1e078125c0dba1cd244d80c2c1192e434989
ttdly <ttdlyu@163.com>
2025-07-05 11:37:15 +0800

    fix(application/client/web): 大模型信息框组件提交，学校 logo 提交
    Signed-off-by: ttdly <ttdlyu@163.com>

---
+++ application/client/web/src/components/live/LLMMessage.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    htmlContent: String
+  }
+}
+</script>
+<template>
+  <div class="llm-msg bg-blue-100 p-2 rounded" v-html="htmlContent"/>
+</template>
+
+<style >
+@reference "@/assets/style/main.css"
+.llm-msg {
+  @apply bg-blue-300 p-2 box-border !important;
+}
+
+.llm-msg h1,h2,h3,h4,h5,h6 {
+  @apply font-bold;
+}
+
+.llm-msg ul li{
+  list-style: disc inside;
+}
+
+.llm-msg pre code {
+  @apply bg-gray-200 rounded box-border;
+}
+</style>
